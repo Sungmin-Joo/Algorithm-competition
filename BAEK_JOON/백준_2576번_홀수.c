ffcd403e4c00b6c03d4b7b42ec0c2e24a52e9058
Sung-Min Joo <wntjdals2015@naver.com>
2019-03-25 17:50:10 +0900

    Create 백준_2576번_홀수.c

---
+++ BAEK_JOON/백준_2576번_홀수.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>  
+
+int main()
+{
+	int sum=0,min=99,i=0,temp[7];
+	for (i = 0; i < 7; i++) {
+		scanf("%d", &temp[i]);
+	}
+	for (i = 0; i <7; i++){
+		if (temp[i] % 2){
+			sum += temp[i];
+			if (min > temp[i]){
+				min = temp[i];
+			}
+		}
+	}
+	if(sum == 0){
+		printf("%d\n", -1);
+	}
+	else{
+		printf("%d\n%d\n", sum, min);
+	}
+	return 0;
+}
