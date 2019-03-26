dfcee3262216b9c340f390bc6e6da08e38461794
Sung-Min Joo <wntjdals2015@naver.com>
2019-03-26 22:38:37 +0900

    Create 백준_2869번_달팽이.c

---
+++ BAEK_JOON/C/백준_2869번_달팽이.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h> \
+
+int main(){
+	int go, back, peek, one_day, temp, mod, Day;
+
+	scanf("%d %d %d", &go, &back, &peek);
+
+	one_day = go - back;
+	temp = peek - go;
+	Day = temp / one_day;
+	mod = temp % one_day;
+
+	Day = (mod) ? Day + 2 : Day + 1;
+	printf("%d\n", Day);
+
+	return 0;
+}
