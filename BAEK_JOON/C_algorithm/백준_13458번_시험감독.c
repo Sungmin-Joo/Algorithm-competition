0453e1428e8f4a481f17c81b6be7c5f916cdc0e2
Sung-Min Joo <wntjdals2015@naver.com>
2019-03-28 14:26:40 +0900

    Algorithm

---
+++ BAEK_JOON/C_algorithm/백준_13458번_시험감독.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h> 
+#include <stdlib.h>
+int i = 0, N, B, C, *A;
+long long sum = 0;
+int main() {
+	scanf("%d", &N);
+	A = (unsigned long long*)malloc(sizeof(unsigned long long) * N);
+	for (i = 0; i<N; i++)
+	{
+		scanf("%d", &A[i]);
+	}
+	scanf("%d %d", &B, &C);
+	
+	for (i = 0; i < N; i++)
+	{
+		sum++;
+		A[i] = A[i] - B;
+		if (A[i] < 0) { continue; }
+		if (A[i] % C){	sum += (A[i] / C) + 1;	}
+		else{	sum += (A[i] / C);	}
+	
+	}
+	printf("%llu\n", sum);
+	free(A);
+	return 0;
+}
