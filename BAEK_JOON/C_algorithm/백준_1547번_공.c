d68106139f9da7c85f41ad68e33aa334526e7f52
Sung-Min Joo <wntjdals2015@naver.com>
2019-04-01 15:23:08 +0900

    Algorithm

---
+++ BAEK_JOON/C_algorithm/백준_1547번_공.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+int main() {
+	int  N, a, b, ball = 1;
+	scanf("%d", &N);
+	while(N--)
+	{
+		scanf("%d %d", &a, &b);
+		if (a == ball)
+		{
+			ball = b;
+		}
+		else if(b == ball)
+		{
+			ball = a;
+		}
+	}
+	printf("%d\n",ball);
+	return 0;
+}
