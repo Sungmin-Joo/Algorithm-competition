d3bed9d9b7bbcd6e17552c32e523e80dd44ef03a
Sung-Min Joo <wntjdals2015@naver.com>
2019-04-01 15:11:13 +0900

    Algorithm

---
+++ BAEK_JOON/C_algorithm/백준_10178번_할로윈 사탕.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+int main() {
+	int  N, a, b;
+	scanf("%d", &N);
+	while(N--)
+	{
+		scanf("%d %d", &a, &b);
+		printf("You get %d piece(s) and your dad gets %d piece(s).\n", a / b, a%b);
+	}
+	return 0;
+}
