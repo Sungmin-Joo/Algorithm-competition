a62877311c439a0a6e41c428eb14d386ef393871
Sung-Min Joo <wntjdals2015@naver.com>
2019-04-05 13:43:37 +0900

    Algorithm

---
+++ BAEK_JOON/C_algorithm/백준_2193번_이친수.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#define ull unsigned long long
+
+ull fibo(ull n)
+{
+	ull temp,i,fi[2] = { 1, 1 };
+	if (n <= 2)
+		return fi[n - 1];
+	else
+	{
+		for (i = 0; i < n - 2 ; i++)
+		{
+			temp = fi[1];
+			fi[1] = fi[0] + fi[1];
+			fi[0] = temp;
+		}
+		return fi[1];
+
+	}
+}
+int main() {
+	ull n;
+	scanf("%d", &n);
+	printf("%llu\n",fibo(n));
+	return 0;
+}
---
+++ BAEK_JOON/C_algorithm/백준_4344번_평균은 넘겠지.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int c, n, s, i, *stu;
+	double sum, cnt = 0;
+	scanf("%d", &c);
+	while (c != 0)
+	{
+		sum = 0;
+		cnt = 0;
+		scanf("%d", &n);
+		stu = (int *)calloc(n, sizeof(int));
+		for (i = 0; i < n; i++)
+		{
+			scanf("%d", &s);
+			stu[i] = s;
+			sum += s;
+		}
+		sum = sum / n;
+		for (i = 0; i < n; i++)
+		{
+			if (stu[i] > sum)
+				cnt += 1;
+		}
+		printf("%.3f%%\n", (100*cnt / n));
+		free(stu);
+		c--;
+	}
+	return 0;
+}
