0f8cbad62e6c684a8d6e93f74f05922cbffde2e4
Sung-Min Joo <wntjdals2015@naver.com>
2019-04-07 01:37:01 +0900

    Algorithm

---
+++ BAEK_JOON/C_algorithm/백준_1065번_한수.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+int main() {
+	int n,i,a,b,c,cnt = 0;
+	scanf("%d", &n);
+	if (n < 100)
+		cnt = n;
+	else
+	{
+		for (i = 100; i <= n; i++)
+		{
+			a = i % 10;
+			b = (i % 100) / 10;
+			c = i / 100;
+			if ((c - b) == (b - a))
+				cnt++;
+		}
+		cnt += 99;
+	}
+	printf("%d", cnt);
+	return 0;
+}
