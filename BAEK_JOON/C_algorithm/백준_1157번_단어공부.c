e4a58d878882986e6b2157ce674bb6366b53f8b7
Sung-Min Joo <wntjdals2015@naver.com>
2019-04-07 21:31:47 +0900

    Algorithm

---
+++ BAEK_JOON/C_algorithm/백준_1157번_단어공부.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <string.h>
+int count[26] = { 0 };
+int main() {
+	char str[1000000],flag = 0,*ptr;
+	int len,i,max=0,index=0;
+	scanf("%s", str);
+	len = strlen(str);
+	ptr = str;
+	for(i =0 ; i < len; i++)
+	{
+		if (*ptr >= 97 && *ptr <= 122)
+			count[(int)(*ptr - 97)]++;
+		else
+			count[(int)(*ptr - 65)]++;
+		ptr++;
+	}
+	for (i = 0; i < 26; i++)
+	{
+		if (count[i] == max)
+		{
+			flag = 1;
+		}
+		else if (count[i] > max)
+		{
+			max = count[i];
+			index = i;
+			flag = 0;
+		}
+	}
+
+	if (flag)
+		putchar('?');
+	else
+		putchar(index+65);
+	return 0;
+}
