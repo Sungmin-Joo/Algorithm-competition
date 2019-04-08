2b42e32ceb323f7485c9fae5d1985719c392de8d
Sung-Min Joo <wntjdals2015@naver.com>
2019-04-08 15:38:31 +0900

    Algorithm

---
+++ BAEK_JOON/Cpp/백준_1978번_소수찾기.cpp
@@ -0,0 +1,31 @@
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+	int temp, cnt = 0;
+	while(n--)
+	{
+		cin >> temp;
+		if (temp == 1)
+			continue;
+		else
+		{
+			for (int i = temp / 2; i > 0; i--)
+			{
+				if (temp % i == 0)
+				{
+					if (i == 1)
+						cnt++;
+					else
+						break;
+				}
+		
+			}
+		}
+	}
+	cout << cnt << endl;
+	return 0;
+}
