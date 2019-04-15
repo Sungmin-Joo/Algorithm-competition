6bd26305e3e345baaac983d6a6cb23ab0a8e32f9
Sung-Min Joo <wntjdals2015@naver.com>
2019-04-15 14:19:59 +0900

    Algorithm

---
+++ BAEK_JOON/C_algorithm/백준_10817번_세 수.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int A, B, C;
+	scanf("%d %d %d",&A, &B, &C);
+	if ((A >= B && A <= C) || (A <= B && A >= C))
+		printf("%d", A);
+	else if ((B >= A && B <= C) || (B <= A && B >= C))
+		printf("%d", B);
+	else
+		printf("%d", C);
+	return 0;
+}
