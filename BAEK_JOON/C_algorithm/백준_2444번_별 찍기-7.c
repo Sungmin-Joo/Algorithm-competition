91868a9b2ff5ebfcbfbb4d9bfa350920d2e74d83
Sung-Min Joo <wntjdals2015@naver.com>
2019-04-20 14:02:16 +0900

    Algorithm

---
+++ BAEK_JOON/C_algorithm/백준_2444번_별 찍기-7.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+void main() {
+	int i, j, n;
+
+	scanf("%d", &n);
+
+	for (i = 1; i <= 2 * n - 1; i++)
+	{
+		if (i <= n)
+		{
+			for (j = 0; j < n - i; j++)
+				putchar(' ');
+			for (j = 0; j < (2 * i) - 1; j++)
+				putchar('*');
+		}
+		else
+		{
+			for (j = 0; j < i - n; j++)
+				putchar(' ');
+			for (j = 0; j < ((2 * n) - 1) - 2 * (i - n); j++)
+				putchar('*');
+		}
+		putchar('\n');
+	}
+}
