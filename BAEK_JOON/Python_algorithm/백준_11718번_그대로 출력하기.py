5398a6d7667a3933942109e6e6270fadc9089158
Sung-Min Joo <wntjdals2015@naver.com>
2019-07-15 12:30:27 +0900

    Update 백준_11718번_그대로 출력하기.py

--- BAEK_JOON/Python_algorithm/백준_11718번_그대로 출력하기.py
+++ BAEK_JOON/Python_algorithm/백준_11718번_그대로 출력하기.py
@@ -1,3 +1,8 @@
-import sys
-a = sys.stdin.read()
-sys.stdout.write(a)
+# -*- coding: utf-8 -*-
+while(1):
+    try:
+        text = input()
+    except EOFError:
+        break
+    else:
+        print (text)
