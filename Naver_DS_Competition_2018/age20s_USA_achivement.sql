5183b556bffba33569a25edf597b3327a03f382f
Sung-Min Joo <wntjdals2015@naver.com>
2019-08-26 02:06:39 +0900

    update

--- Naver_DS_Competition_2018/age20s_USA_achivement.sql
+++
@@ -1,5 +0,0 @@
-SELECT Country.countryName, Age.howOld, Category.categoryName
-FROM User JOIN Country JOIN Age JOIN Category 
-ON User.ages_id=Age.id AND User.country_id=Country.id AND User.category_id = Category.id 
-WHERE Age.howOld>=20 AND Age.howOld<30 AND Country.countryName = 'USA' AND Category.categoryName = 'achievement'
-ORDER BY Age.howOld DESC, Country.countryName
