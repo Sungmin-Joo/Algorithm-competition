09dc29ed729f26cb31bd10c02bce56da1c7c4c79
Sung-Min Joo <big-joo_dev@naver.com>
2019-09-25 12:25:14 +0900

    update 09/25

---
+++ BAEK_JOON/Python_algorithm/백준_1654번_랜선자르기.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+def solution(k,n, arr):
+    high = arr[0]
+    low = 0
+    result = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+        cnt = 0
+        if mid == 0: return 1
+        for val in arr:
+            cnt += val//mid
+            if cnt >= n and mid > result:
+                result = mid
+                break
+
+        if cnt < n:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return  result
+
+
+if __name__ == "__main__":
+    K, N = map(int,input().split())
+    arr = []
+
+    for _ in range(K):
+        arr.append(int(input()))
+    arr.sort(reverse = True)
+
+    print(solution(K, N, arr))
